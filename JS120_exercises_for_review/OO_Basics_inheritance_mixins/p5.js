c3c627fe7678eb45105795314d96928735275680
Michael S <mshyer@outlook.com>
2023-01-08 10:29:12 -0500

    exercises

---
+++ JS120_exercises_for_review/OO_Basics_inheritance_mixins/p5.js
@@ -0,0 +1,23 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    return `Hello! My name is ${this.name}!`;
+  }
+}
+
+const walkMixin = {
+  walk() {
+    return 'Let\'s go for a walk.';
+  },
+};
+
+Object.assign(Cat.prototype, walkMixin);
+
+console.log(walkMixin.walk());
+
+let kitty = new Cat("Sophie");
+console.log(kitty.greet());
+console.log(kitty.walk());
---
+++ JS120_exercises_for_review/OO_Basics_inheritance_mixins/p7.js
@@ -0,0 +1,18 @@
+
+const towMixin = {
+  tow() {
+    return 'I can tow a trailer';
+  }
+};
+
+class Truck {
+  constructor() {
+    Object.assign(this, towMixin);
+  }
+}
+
+class Car {}
+
+
+let truck = new Truck();
+truck.tow();
